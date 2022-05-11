0a70a452e4682e2ac06d31f5710f725681e3311c
Ju Kim <jukim@Jus-MacBook-Pro.local>
2022-05-11 13:07:25 -0700

    dist

---
+++ __tests__/supertest.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const server = "http://localhost:3000";
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * Read the docs! https://www.npmjs.com/package/supertest
+ */
